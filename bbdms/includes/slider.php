9c54bd33c71ee81b95ed43e406cca3532beb87b5
Rukhaiah <141475228+Rukhaiah@users.noreply.github.com>
2024-02-19 20:05:02 +0530

    Add files via upload

---
+++ bbdms/includes/slider.php
@@ -0,0 +1,45 @@
+ <header>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1" ></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="2" ></li>
+                
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <!-- Slide One - Set the background image for this slide in the line below -->
+                <div class="carousel-item active" style="background-image: url('https://www.blood.ca/sites/default/files/styles/max_1300x1300/public/28828450_m.jpg?itok=7QWOQyu4')">
+                    <div class="carousel-caption d-none d-md-block">
+                       
+                    </div>
+                </div>
+		
+		<!-- Slide Two - Set the background image for this slide in the line below -->
+		<div class="carousel-item" style="background-image: url('https://www.essentialmed.co.za/images/donating-blood-banner.jpg')">
+                    <div class="carousel-caption d-none d-md-block">
+                    </div>
+                </div>
+                
+
+                <!-- Slide Three - Set the background image for this slide in the line below -->
+                <div class="carousel-item" style="background-image: url('https://souvikb07.github.io/images/blood_donationcover.jpeg')">
+                    <div class="carousel-caption d-none d-md-block">
+                    </div>
+                </div>
+                
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+	<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+
+        </div>
+    </header>
