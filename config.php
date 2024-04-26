98b8fd1994ff689a4af7e9aca46d0c4e04901f18
Rukhaiah <141475228+Rukhaiah@users.noreply.github.com>
2024-04-26 13:07:48 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','bbdms');
-// Establish database connection.
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "bloodserver.mysql.database.azure.com", "bloodbank", "RukkuSreya@2024", "bloodbank", 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno()) {
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}// Establish database connection.
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
