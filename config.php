eb31a8c9f4e0c5d6e9c057ca31d9ca7d4e3922fe
Rukhaiah <141475228+Rukhaiah@users.noreply.github.com>
2024-04-27 10:36:09 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,16 +2,16 @@
 // DB credentials.
 $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "bloodserver.mysql.database.azure.com", "bloodbank", "RukkuSreya@2024", "bloodbank", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($con, "bloodserver.mysql.database.azure.com", "bloodbank", "RukkuSreya@2024", "bloodbank", 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno()) {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }// Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
-}
+//try
+//{
+//$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+//}
+//catch (PDOException $e)
+//{
+//exit("Error: " . $e->getMessage());
+//}
 ?>
